02b4dbbb4e1a41d63bfae70cb2f23b94915270c3
Volumio <info@volumio.org>
2019-01-10 11:50:44 +0000

    Improvement in buildscript

--- buildscript.sh
+++ buildscript.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 #MPD Volumio Custom recipe compiler script
+echo "Removing upnp dep"
+sudo apt-get remove -y libupnp6
+
 echo "Installing dependencies"
 
-apt-get update
-apt-get install -y build-essential automake autoconf libtool pkg-config libcurl4-openssl-dev intltool libxml2-dev libgtk2.0-dev libnotify-dev libglib2.0-dev libevent-dev dh-make devscripts
-apt-get build-dep -y mpd
+sudo apt-get update
+sudo apt-get install -y build-essential automake autoconf libtool pkg-config libcurl4-openssl-dev intltool libxml2-dev libgtk2.0-dev libnotify-dev libglib2.0-dev libevent-dev dh-make devscripts
+sudo apt-get build-dep -y mpd
 
 echo "Creating Package"
 debuild -i -us -uc -b -d
