4425982b19096703e3aae7d5849f8c3ed444e481
Volumio <info@volumio.org>
2019-01-10 16:15:02 +0000

    Fix script

--- buildscript.sh
+++ buildscript.sh
@@ -3,15 +3,18 @@
 #MPD Volumio Custom recipe compiler script
 echo "Installing dependencies"
 
-apt-get update
-apt-get install -y build-essential automake autoconf libtool pkg-config libcurl4-openssl-dev intltool libxml2-dev libgtk2.0-dev libnotify-dev libglib2.0-dev libevent-dev dh-make devscripts
-apt-get -y remove libupnp6
-apt-get -y install libupnp6
-apt-get build-dep -y mpd
-apt-get -y install libsidplayfp-dev
+sudo apt-get update
+sudo apt-get install -y build-essential automake autoconf libtool pkg-config libcurl4-openssl-dev intltool libxml2-dev libgtk2.0-dev libnotify-dev libglib2.0-dev libevent-dev dh-make devscripts
+sudo apt-get -y remove libupnp6
+sudo apt-get -y install libupnp6
+sudo apt-get build-dep -y mpd
+sudo apt-get -y install libsidplayfp-dev
 
 ## Edit debian/rules by removing dh_strip override
 rm -rf debian/source
+
+echo "Fixing libraries"
+autoreconf -f -i
 
 echo "Creating Package"
 debuild -i -us -uc -b -d
