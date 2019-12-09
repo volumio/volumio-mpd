c42ae04105bc225702a4b2d73fade81e79c6a6a5
Volumio <info@volumio.org>
2019-12-09 12:41:02 +0000

    Adding buildscript

---
+++ buildscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#MPD Volumio Custom recipe compiler script
+echo "Removing upnp dep"
+#sudo apt-get remove -y libupnp6
+
+echo "Installing dependencies"
+
+sudo apt-get update
+sudo apt-get install -y build-essential automake autoconf libtool pkg-config libcurl4-openssl-dev intltool libxml2-dev libgtk2.0-dev libnotify-dev libglib2.0-dev libevent-dev dh-make devscripts libchromaprint-dev libgcrypt20-dev libsidplayfp-dev python3-sphinx libgtest-dev debhelper
+sudo apt-get build-dep -y mpd
+
+echo "Creating Package"
+debuild -i -us -uc -b -d
+
+echo "Done"
