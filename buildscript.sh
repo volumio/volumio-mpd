1ee336816fbd6f2a59c810c3541e0ba45cd4adfd
Volumio <info@volumio.org>
2021-04-15 09:22:49 +0000

    Buildscript

---
+++ buildscript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#MPD Volumio Custom recipe compiler script
+echo "Removing upnp dep"
+#sudo apt-get remove -y libupnp6
+
+
+echo "Remember to uncomment sources line in /etc/apt/sources.list"
+
+echo "Installing dependencies"
+
+sudo apt-get update
+sudo apt-get install -y build-essential automake autoconf libtool pkg-config libcurl4-openssl-dev intltool libxml2-dev libgtk2.0-dev libnotify-dev libglib2.0-dev libevent-dev dh-make devscripts libchromaprint-dev libgcrypt20-dev libsidplayfp-dev python3-sphinx libgtest-dev debhelper
+sudo apt-get install -y meson libboost-dev libsmbclient-dev libasound2-dev libavformat-dev libnfs-dev 
+sudo apt-get build-dep -y mpd
+
+echo "Creating Package"
+debuild -i -us -uc -b -d
+
+echo "Done"
