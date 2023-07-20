a4c8fbfb9355af37db55a0bbf01024a3d6ac61b8
Volumio <info@volumio.org>
2023-07-20 12:42:38 +0200

    Add buildscript

---
+++ buildscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#MPD Volumio Custom recipe compiler script
+echo "Remember to uncomment sources line in /etc/apt/sources.list"
+echo "For PI remove /etc/apt/preferences.d/raspberrypi-kernel"
+
+echo "Installing dependencies"
+
+sudo apt-get update
+sudo apt-get install -y build-essential devscripts debhelper dh-make cmake libfmt-dev libboost-dev
+sudo apt-get build-dep -y mpd
+
+sudo apt-get install -y python3-pip
+sudo pip3 install meson
+
+echo "Creating Package"
+sudo dh binary-arch
+
+echo "Done"
