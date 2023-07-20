7b42f25fd6ad8a956f39c5cd179edec1e7b3e5b9
Volumio <info@volumio.org>
2023-07-20 13:26:38 +0200

    add libpcre2-dev

--- buildscript.sh
+++ buildscript.sh
@@ -7,7 +7,7 @@ echo "For PI remove /etc/apt/preferences.d/raspberrypi-kernel"
 echo "Installing dependencies"
 
 sudo apt-get update
-sudo apt-get install -y build-essential devscripts debhelper dh-make cmake libfmt-dev libboost-dev
+sudo apt-get install -y build-essential devscripts debhelper dh-make cmake libfmt-dev libboost-dev libpcre2-dev
 sudo apt-get build-dep -y mpd
 
 sudo apt-get install -y python3-pip
