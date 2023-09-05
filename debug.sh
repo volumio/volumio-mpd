2b69eaded0347b6cc62c92347f9c4fd0d53f4649
Volumio <info@volumio.org>
2023-09-06 00:42:29 +0200

    Add debug

---
+++ debug.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#only first time 
+#apt-get update
+#apt-get install -y gdb
+
+echo "Attaching to MPD process"
+sudo gdb -p $(pidof mpd)
+
+
+# Once crashed, to see the backtrace
+# bt
+# SEE
+# https://wiki.archlinux.org/title/Core_dump
+
+
